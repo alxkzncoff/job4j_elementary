aa121c059918ab883dcdf038af564ca9a71e1036
Aleksandr <alx.kzncoff@gmail.com>
2021-09-05 14:20:11 +0300

    Checkstyle fix

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -12,7 +12,7 @@ public class Counter {
     public static int sumByEven(int start, int finish) {
         int sum = 0;
         for (int index = start; index <= finish; index++) {
-            if (index%2==0) {
+            if (index % 2 == 0) {
                 sum += index;
             }
         }
@@ -20,9 +20,9 @@ public class Counter {
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(0,10));
-        System.out.println(sum(3,8));
-        System.out.println(sum(1,1));
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
         System.out.println(sum(7, 21));
         System.out.println(sum(100, 200));
 
