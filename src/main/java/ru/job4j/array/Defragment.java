1000c76720756239756cf719da73c517aa040cd5
alxkzncoff <alx.kzncoff@gmail.com>
2021-09-09 14:05:05 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -3,9 +3,9 @@ package ru.job4j.array;
 public class Defragment {
     public static String[] compress(String[] array) {
         for (int nullIndex = 0; nullIndex < array.length; nullIndex++) {
-            if (array[nullIndex] == null) { /* ищем ячейку с null значением */
+            if (array[nullIndex] == null) {
                 for (int notNullIndex = nullIndex + 1; notNullIndex < array.length; notNullIndex++) {
-                    if (array[notNullIndex] != null) { /* ищем ячейку без null значения */
+                    if (array[notNullIndex] != null) {
                         array = SwitchArray.swap(array, nullIndex, notNullIndex);
                         break;
                     }
